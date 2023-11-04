2d5ac73fc8f7dc509b765f347ca3282edb9df118
rahul_rathee7 <ratheerahul602@gmail.com>
2023-11-04 12:19:22 +0530

    new inserted value

--- insertion.cpp
+++ insertion.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int insertion(int arr[], int n, int key, int cap) {
+	if(n >= cap)
+		return n;
+	arr[n] = key;
+	return (n+1);
+}
+
 int main() {
-	int arr[5] = { 2, 4, 1, 3, 5 };
-	int newvalue = 6;
-	arr[5] = newvalue;
-	int size = sizeof(arr)/sizeof(arr[0]);
-	for(int i = 0; i <= size; i++) {
+	int arr[10] = {2, 4, 1, 3, 5};
+	int cap = sizeof(arr)/sizeof(arr[0]);
+	int n = 5;
+	int key = 26;
+	n = insertion(arr, n, key, cap);
+	for(int i = 0; i < 6; i++) {
 		cout << arr[i] << endl;
 	}
 	return 0;
