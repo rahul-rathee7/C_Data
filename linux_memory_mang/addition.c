ab1cf01d8aea74cb80d8992fe001a6c557f1d056
rahul-rathee7 <ratheerahul602@gmail.com>
2023-12-12 12:30:53 +0530

    commit on 12-12-2023

--- linux_memory_mang/addition.c
+++ linux_memory_mang/addition.c
@@ -2,7 +2,10 @@
 
 int main() {
 	int a, b;
-	scanf("%d %d",&a, &b);
-	printf("%d\n",a+b);
+	printf("Please enter first number: ");
+	scanf("%d",&a);
+	printf("Please enter second number: ");
+	scanf("%d",&b);
+	printf("addition of %d and %d is %d\n",&a,&b,a+b);
 	return 0;
 }
